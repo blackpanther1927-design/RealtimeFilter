7a6a8e3975cd6346e5a7d481bde8f21e4e30ceb5
blackpanther1927-design <83531534+blackpanther1927-design@users.noreply.github.com>
2021-05-02 13:03:10 +0530

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,16 @@
+function prelooad() {
+
+}
+
+function setup() {
+    canvas=createCanvas(300 , 300);
+    canvas.center();
+}
+
+function draw(){
+
+}
+
+function takeSnapshot() {
+ save('MyFilterImage.png')
+}
